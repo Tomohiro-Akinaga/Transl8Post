33ca6f59fd33ae54d188b8cdd11c1542005eff3a
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-02 17:05:52 +0900

    CMSへGetリクエスト

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Page = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default Page;
