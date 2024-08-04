140059382a7cdec22a4631b6a54d17af2763d985
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-04 20:34:41 +0900

    記事作成ボタンを配置

---
+++ frontend/src/components/atoms/TextButton/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React, { ComponentPropsWithoutRef } from "react";
+
+interface Props extends ComponentPropsWithoutRef<"button"> {}
+
+export const TextButton = ({ children, onClick }: Props) => {
+  return <button onClick={onClick}>{children}</button>;
+};
--- frontend/src/components/organisms/Header/index.tsx
+++ frontend/src/components/organisms/Header/index.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import React from "react";
 import styles from "./index.module.css";
+import { TextButton } from "@/components/atoms/TextButton";
 
 export const Header = () => {
+  const handleClick = () => {
+    console.log("ブログ作成ボタンがクリックされました");
+    /*
+     ** TODO: 作成ボタン押下でブログ作成ページに遷移
+     */
+  };
   return (
     <header className={styles.header}>
       <h1>Trans8Post</h1>
+      <TextButton onClick={handleClick}>記事を作成</TextButton>
     </header>
   );
 };
