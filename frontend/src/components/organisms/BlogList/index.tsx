d3cb2e38d85cb4e14b0dbfc3d588bfc0887b6eaf
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-05 15:24:25 +0900

    Update index.tsx

--- frontend/src/components/organisms/BlogList/index.tsx
+++ frontend/src/components/organisms/BlogList/index.tsx
@@ -12,20 +12,24 @@ interface Props {
 
 export const BlogList = () => {
   const [blogList, setBlogList] = useState<Props[]>([]);
+  const [loadingText, setLoadingText] = useState<string>("");
 
   useEffect(() => {
+    setLoadingText("読み込み中...");
     const fetchBlogList = async () => {
       const response = await client.getAllContents({
         customRequestInit: { cache: "no-store" },
         endpoint: "blog",
       });
       setBlogList(response);
+      setLoadingText("");
     };
     fetchBlogList();
   }, []);
 
   return (
     <ul className={styles.list}>
+      <p>{loadingText}</p>
       {blogList.map((v, i) => (
         <BlogItem title={v.title} body={v.body} key={i} />
       ))}
