5e980eeeacd649074c562f95f9de952f04298884
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-08 15:51:02 +0900

    Update route.ts

--- frontend/src/app/api/POST/route.ts
+++ frontend/src/app/api/POST/route.ts
@@ -5,9 +5,12 @@ export async function POST(req: NextRequest) {
   const { title, text, translatedTitle, translatedText } = await req.json();
   const response = await client.create({
     endpoint: "blog",
-    content: JSON.parse(
-      `{"title":"${title}", "text":"${text}", "translatedTitle":"${translatedTitle}", "translatedText":"${translatedText}"}`
-    ),
+    content: {
+      title: title,
+      text: text,
+      translatedTitle: translatedTitle,
+      translatedText: translatedText,
+    },
   });
 
   return NextResponse.json(response);
