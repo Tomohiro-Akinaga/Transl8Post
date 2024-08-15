607a147b4d64be207cef0d6e6a0c54016bbbae1f
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-16 00:05:05 +0900

    Update client.ts

--- frontend/libs/client.ts
+++ frontend/libs/client.ts
@@ -1,7 +1,7 @@
-import { createClient } from "microcms-js-sdk";
+import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
-  serviceDomain: process.env.NEXT_PUBLIC_CMS_SERVICE_DOMAIN ?? "",
-  apiKey: process.env.NEXT_PUBLIC_CMS_API_KEY ?? "",
+  serviceDomain: process.env.CMS_SERVICE_DOMAIN ?? '',
+  apiKey: process.env.CMS_API_KEY ?? '',
   retry: true,
-});
+})
