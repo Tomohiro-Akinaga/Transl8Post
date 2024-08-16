1c076f3a5db20d623574f259f784a1511b39222d
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-16 18:50:58 +0900

    Create middleware.ts

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { getSession } from '@auth0/nextjs-auth0/edge'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const session = await getSession(req, res)
+  const userId = session?.user.sub
+  if (!userId) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+}
+
+export const config = {
+  matcher: ['/', '/edit'],
+}
