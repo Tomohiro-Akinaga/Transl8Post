6d8c3be80c14ba1406340a12b1a9c37922fadf4d
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-19 17:37:13 +0900

    Update route.ts

--- frontend/src/app/api/GET/route.ts
+++ frontend/src/app/api/GET/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { client } from '../../../../libs/client'
 
 export async function GET() {
-  const response = await client.getAllContents({ endpoint: 'contents' })
+  const response = await client.getAllContents({ endpoint: 'blog' })
 
   return NextResponse.json(response)
 }
