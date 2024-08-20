6cf953798c87244b811e1f4cbc73a8ed608f2ada
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-08-20 12:12:32 +0900

    スタイル修正

--- frontend/src/components/atoms/SiteLogo/index.tsx
+++ frontend/src/components/atoms/SiteLogo/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.module.css'
 const SiteLogo = () => {
   return (
     <Link className={styles.link} href={'/'}>
-      Transl8Post
+      TLANSL8POST
     </Link>
   )
 }
